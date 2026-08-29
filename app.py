65d6f1c2d9c81ecbba14ca9ec77acf00f07f940f
officialvedant19-ship-it <officialvedant19@gmail.com>
2026-08-29 07:09:36 +0530

    Fixed port binding for Render

--- app.py
+++ app.py
@@ -614,5 +614,5 @@ def health_check():
 
 # ---------------- Run ----------------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
